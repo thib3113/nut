1c63d6baafeadd864548bd4e528a3208c7c98a44
thibaut severac <thib3113@gmail.com>
2024-10-26 20:35:34 +0200

    fix(CI)

--- tests/RawNUTClient.tests.ts
+++ tests/RawNUTClient.tests.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it } from '@jest/globals';
-import { RawNUTClient } from '../src/RawNUTClient.js';
+import { RawNUTClient } from '../index.js';
 import { setTimeout } from 'node:timers/promises';
 import { TLSSocket } from 'tls';
 import { setInterval } from 'timers/promises';
